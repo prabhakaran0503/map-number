f88473b93d64029ccac65edeff8dbaf1612441c9
prabhakaran <prabhakaranplkk@gmail.com>
2023-09-03 21:38:12 +0530

    map

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var input=[5,10,15,20];
+
+var ans=input.map(function(value,index){
+    //  console.log(value);
+    var a
+        if(15<=value){
+        a=value*2
+            
+        }
+        return a
+})
+console.log(ans);
